41acf5c93ed32a5d6be4d44305f3684398673673
yash5chandrakar <yash5chandrakar@gmail.com>
2022-10-31 23:14:52 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 
-
-
 function App() {
   const initialState = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
   const [gameBoard, setGameBoard] = useState(initialState)
